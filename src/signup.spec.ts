993031422c2d4c00ee926dd35bd1f38b9fc9a22d
ricardotavaresit <ricardotavaresit@gmail.com>
2020-09-19 11:32:35 +0100

    test: ensure SignUpController return an error if no name is provided

--- src/signup.spec.ts
+++ src/signup.spec.ts
@@ -12,7 +12,8 @@ describe('SignUp Controller', () => {
       }
       const httpResponse = sut.handle(httpRequest); 
       expect(httpResponse.statusCode).toBe(400);
+      expect(httpResponse.body).toEqual( new Error('Missing param: name'));
 
-      expect(1).toBe(1)
+    
    })
 })
