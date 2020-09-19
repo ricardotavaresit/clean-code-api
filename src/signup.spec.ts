9272f1f99fa752cd2e6ab5352a41aff0cbdec672
ricardotavaresit <ricardotavaresit@gmail.com>
2020-09-19 09:38:25 +0100

    test: ensure SignUpController returns 400 if no name

--- src/signup.spec.ts
+++ src/signup.spec.ts
@@ -13,8 +13,6 @@ describe('SignUp Controller', () => {
       const httpResponse = sut.handle(httpRequest); 
       expect(httpResponse.statusCode).toBe(400);
 
-
-
       expect(1).toBe(1)
    })
 })
